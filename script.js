3fb580997f087d2ec27c7e9fa5d9fbf8c5583852
gsingh1017 <182170594+gsingh1017@users.noreply.github.com>
2024-11-28 18:24:16 -0800

    Add functions resetCalculator, operate, and buttonInterface
    Added a resetCalculator function to reset values when AC clicked. Operate function calls add, subtract, multiply, or divide accordingly. Added button interface similar to Google's calculator stored in buttonInterface function.

--- script.js
+++ script.js
@@ -6,6 +6,27 @@ const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
+const display = document.querySelector("#display");
+const buttons = document.querySelectorAll("button");
+
+const equalSign = document.querySelector("#equalSign");
+const clear = document.querySelector(`button[value = "clear"]`);
+
+// Initializer
+let a = "";
+let b = "";
+let operator = "";
+let total = 0;
+
+
+function resetCalculator() {
+    a = "";
+    b = "";
+    operator = "";
+    total = 0;
+    display.textContent = "0";
+}
+
 
 function operate(a, operator, b) {
     if (operator === "+") {
@@ -26,15 +47,50 @@ function operate(a, operator, b) {
 }
 
 
-// Changes button colors when mouse hovers over
-const changeButtonColor = document.querySelector("button");
-changeButtonColor.addEventListener("mouseover", (event) => {
-    event.target.style.backgroundcolor = 'rgb(78, 142, 184)';
+// Feedback when mouse interacts with buttons
+function buttonInterface() {
+
+    buttons.forEach((element) => {
+
+        element.addEventListener("mouseenter", () => {
+            element.style.opacity = 0.9;
+        });
+    
+        element.addEventListener("mouseleave", () => {
+            element.style.opacity = 1;
+        });
+    
+        element.addEventListener("mousedown", () => {
+            element.style.opacity = 0.9;
+            display.style.border = "1px rgb(130, 130, 130) solid";
+        });
+    
+        element.addEventListener("mouseup", () => {
+            element.style.opacity = 1;
+            display.style.border = "1px rgb(127, 255, 212) solid";
+        });
+    });
+}
+
+
+buttons.forEach((element) => {
+    element.addEventListener("click", () => {
+        a = a + element.textContent; 
+        display.textContent = a;  
+        console.log(a); 
+    });
 });
 
-changeButtonColor.addEventListener("mousedown", (event) => {
-    event.target.style.border = "1px solid white";
+
+equalSign.addEventListener("click", () => {
+    total = operate(a, operator, b);
+    display.textContent = String(total);
 });
 
 
-const display = document.querySelector("#display");
+// resetCalculator called when "AC" is clicked
+clear.addEventListener("click", () => {
+    resetCalculator();
+});
+
+buttonInterface();
